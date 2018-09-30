b0a79f73213fa61c9b4cf6f95b360e8e160553f2
Gavan Lamb <gavan.jon.lamb@gmail.com>
2018-09-30 19:48:55 +1000

    Feature/add pin setup (#2)
    * Update git ignore
    * add pin config to yml file
    * Add setup method
    * add conf1 file

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,45 @@
  */
 
 #include "mgos.h"
+#include "mgos_gpio.h"
+#include "mgos_mqtt.h"
+#include "mgos_sys_config.h"
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stdint.h>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+//SETUP SECTION
+static vector<uint16_t> relays;
+static uint8_t number_of_relays_to_control;
+
+void setup_switches()
+{
+    istringstream string_relays(mgos_sys_config_get_bank_relay_pins());
+
+    string relay;
+
+    while (getline(string_relays, relay, ',')) {
+        int relay_pin_number;
+        istringstream convert(relay);
+        convert >> relay_pin_number;
+        mgos_gpio_set_mode(relay_pin_number, MGOS_GPIO_MODE_OUTPUT);
+        relays.push_back(relay_pin_number);
+    }
+
+    number_of_relays_to_control = relays.size();
+}
+
+void setup()
+{
+    setup_switches();
+}
 
 enum mgos_app_init_result mgos_app_init(void)
 {
+    setup();
     return MGOS_APP_INIT_SUCCESS;
 }
